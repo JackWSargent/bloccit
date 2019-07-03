36267bac0aebd8c48bbef8e736d9b1ba2144f541
JackWSargent <jackwsargent@gmail.com>
2019-07-03 04:25:43 -0400

    completed assignment

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
 
+app.use("/marco", (req, res, next) => {
+    res.send("polo")
+});
+
 routeConfig.init(app);
 
 module.exports = app;
